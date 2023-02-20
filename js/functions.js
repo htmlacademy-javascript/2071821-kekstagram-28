04be983b5af4291bbb66dafceda794bca515686c
Olga Fabritsius <olga.fabrizius@gmail.com>
2023-02-20 15:16:39 +0100

    feat: adds function that extracts  number

--- js/functions.js
+++ js/functions.js
@@ -8,3 +8,32 @@ function verifyLength(string, referenceLenth) {
 }
 
 verifyLength('Good morning', 10);
+/*
+function isPalindrome(string) {
+  let i = 0;
+  while (i < string.length / 2) {
+    if(string[i] === string[string.length - (i + 1)]){
+      i++;
+      console.log(i);
+    }
+    console.log('not');
+    break;
+  }
+  console.log('palindrome');
+}
+
+isPalindrome('abracadabra');
+*/
+
+function getNumber(string) {
+  let number = '';
+  string = string + '';
+  for(let i = 0; i < string.length; i++){
+    const character = parseInt(string[i], 10);
+    if (!isNaN(character)) {
+      number = number + character;
+    }
+  }
+  return parseInt(number, 10);
+}
+console.log(getNumber(2023));
