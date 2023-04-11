d729eff59e2547ffe31cce9467260081594edc9d
Olga Fabritsius <olga.fabrizius@gmail.com>
2023-04-11 16:38:29 +0200

    fix: solves problem with closing message

--- js/messages.js
+++ js/messages.js
@@ -1,66 +1,50 @@
 import { isEscapeKey, showAlert } from './utils.js';
+
 const successMessageTemplate = document.querySelector('#success').content.querySelector('.success');
 const successMessageBlock = successMessageTemplate.cloneNode(true);
-const successButton = successMessageBlock.querySelector('.success__button');
-
 const errorMessageTemplate = document.querySelector('#error').content.querySelector('.error');
 const errorMessageBlock = errorMessageTemplate.cloneNode(true);
-const failButton = errorMessageBlock.querySelector('.error__button');
 
-const onDocumentKeydown = (evt) => {
+let currentMessageBlock = null;
+
+const showMessageBlock = () => {
+  document.body.append(currentMessageBlock);
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const hideMessageBlock = () => {
+  currentMessageBlock.remove();
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+const onModalClick = (evt) => {
+  if (typeof evt.target.dataset.close !== 'undefined') {
+    hideMessageBlock();
+  }
+};
+
+function onDocumentKeydown (evt) {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
-    if (errorMessageBlock) {
-      hideFailMessage();
-    }
-    hideSuccessMessage();
+    hideMessageBlock();
   }
-};
-
-const onDocumentClick = (evt) => {
-  const { close, modal } = evt.target.dataset;
-  if (close || modal) {
-    if (errorMessageBlock) {
-      hideFailMessage();
-    }
-    hideSuccessMessage();
-  }
-};
-
-
-function hideSuccessMessage () {
-  successMessageBlock.remove();
-  document.removeEventListener('keydown', onDocumentKeydown);
-  document.removeEventListener('click', onDocumentClick);
 }
-
-function hideFailMessage () {
-  errorMessageBlock.remove();
-  document.removeEventListener('keydown', onDocumentKeydown);
-  document.removeEventListener('click', onDocumentClick);
-}
-
-const onSuccessButtonClick = () => hideSuccessMessage();
-const onFailButtonClick = () => hideFailMessage();
-
 
 const showSuccessMessage = () => {
-  successButton.addEventListener('click', onSuccessButtonClick);
-  document.body.append(successMessageBlock);
-  document.addEventListener('keydown', onDocumentKeydown);
-  document.addEventListener('click', onDocumentClick);
+  currentMessageBlock = successMessageBlock;
+  showMessageBlock();
 };
 
 const showFailMessage = () => {
-  failButton.addEventListener('click', onFailButtonClick);
-  document.body.append(errorMessageBlock);
-  document.addEventListener('keydown', onDocumentKeydown);
-  document.addEventListener('click', onDocumentClick);
+  currentMessageBlock = errorMessageBlock;
+  showMessageBlock();
 };
+
+successMessageBlock.addEventListener('click', onModalClick);
+errorMessageBlock.addEventListener('click', onModalClick);
 
 const onUploadFail = () => {
   showAlert('Ошибка загрузки данных');
 };
-
 
 export { showFailMessage, showSuccessMessage, onUploadFail };
